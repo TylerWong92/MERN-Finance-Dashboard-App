47db7f0e1e3709346250698c5d4939440f24c50e
tyler <tyler.tongseng@gmail.com>
2023-04-03 13:07:35 +0800

    Spilt Dashboard content to 3 pages

--- client/src/components/scenes/dashboard/index.tsx
+++ client/src/components/scenes/dashboard/index.tsx
@@ -1,6 +1,8 @@
-import { Box, useTheme } from "@mui/system";
+import { Box } from "@mui/system";
 import { useMediaQuery } from "@mui/material";
-import DashboardBox from "@/components/DashboardBox";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 type Props = {};
 
@@ -49,9 +51,9 @@ const gridTemplateSmallScreens = `
     "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
-  const { palette } = useTheme();
+  // const { palette } = useTheme();
   return (
     <Box
       width="100%"
@@ -72,16 +74,9 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
