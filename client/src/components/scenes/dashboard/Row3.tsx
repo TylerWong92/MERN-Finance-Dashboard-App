b0b3ca290d77010e9d235079a765fffaa72fa852
tyler <tyler.tongseng@gmail.com>
2023-04-09 17:53:14 +0800

    Simple Bar chart using box

--- client/src/components/scenes/dashboard/Row3.tsx
+++ client/src/components/scenes/dashboard/Row3.tsx
@@ -181,7 +181,33 @@ const Row3 = () => {
           })}
         </FlexBetween>
       </DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="j">
+        <BoxHeader
+          title="Overall Summary and Explanation Data"
+          sideText="+4%"
+        />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            margin="0rem 0rem 0.5rem 0rem"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          ></Box>
+
+          <Typography margin="0.1rem" variant="h6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis
+            reiciendis minima exercitationem ipsa pariatur nesciunt.
+            Perferendis, iure error nemo repellat magni minima quaerat ullam
+            exercitationem necessitatibus libero ex voluptates omnis.
+          </Typography>
+        </Box>
+      </DashboardBox>
     </>
   );
 };
